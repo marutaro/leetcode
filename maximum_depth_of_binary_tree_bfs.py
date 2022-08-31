8c342110b45d0f85a85daf0438183f73d4ae4a4b
marutaro <niits.git@gmail.com>
2022-08-31 12:16:57 +0900

    Create maximum_depth_of_binary_tree_bfs.py

---
+++ maximum_depth_of_binary_tree_bfs.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
