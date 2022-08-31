35eecb7a9cf5911106630971baaac7f4ee3f3245
marutaro <niits.git@gmail.com>
2022-08-31 12:18:12 +0900

    Delete maximum_depth_of_binary_tree_bfs.py

--- maximum_depth_of_binary_tree_bfs.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
