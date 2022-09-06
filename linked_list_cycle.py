67e25998fac9d93075db3d311a129d6c38b48a91
marutaro <niits.git@gmail.com>
2022-09-07 04:42:13 +0900

    Create linked_list_cycle.py

---
+++ linked_list_cycle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+            if fast == slow:
+                return True
+        
+        return False
