9105569f8d56a27d13a33fb0c4ac44e705466d18
marutaro <niits.git@gmail.com>
2022-11-07 02:21:33 +0900

    Create partition_list.py

---
+++ partition_list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+
+        slist, blist = ListNode(), ListNode()
+        small, big = slist, blist # dummy lists
+
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                big.next = head
+                big = big.next
+
+            head = head.next
+
+        small.next = blist.next
+        big.next = None # prevent linked list circle
+
+        return slist.next
