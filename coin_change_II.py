ff8028fb9eca535dfb1a8a685604810eadb41eed
marutaro <niits.git@gmail.com>
2022-11-17 03:41:27 +0900

    Create coin_change_II.py

---
+++ coin_change_II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        dp = [0] * (amount + 1) # [0,0,0,0,0]
+        dp[0] = 1
+
+        for c in coins:
+            for a in range(1, amount + 1):
+                if a - c >= 0:
+                    dp[a] += dp[a-c]
+        
+        return dp[amount]
