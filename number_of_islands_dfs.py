98c28f12f57a17a2a051117f1fc8d7f5d004756f
marutaro <niits.git@gmail.com>
2022-12-04 11:24:52 +0900

    Update number_of_islands_dfs.py

--- number_of_islands_dfs.py
+++ number_of_islands_dfs.py
@@ -6,20 +6,15 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
 
         def dfs(r, c):
-             q = deque()
-             visited.add((r,c))
-             q.append((r,c))
-           
-             while q:
-                 row,col = q.popleft()
-                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
-               
-                 for dr,dc in directions:
-                     r,c = row + dr, col + dc
-                     if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
-                       
-                         q.append((r , c ))
-                         visited.add((r, c ))
+            if r not in range(rows) or c not in range(cols) or grid[r][c] == "0" or (r, c) in visited:
+                return
+            
+            visited.add((r, c))
+
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
 
         for r in range(rows):
             for c in range(cols):
