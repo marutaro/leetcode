68c32e3c9d6cd8b087cf46555687a0f0d327fdbd
marutaro <niits.git@gmail.com>
2022-12-17 18:21:21 +0900

    Create plus_one.py

---
+++ plus_one.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        for i in range(len(digits) - 1, -1, -1):
+
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+
+            if i == 0:
+                return [1] + digits
