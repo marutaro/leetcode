55bc6b7134fd95a1d7ec69e3ce14645e03da00cc
marutaro <niits.git@gmail.com>
2022-12-22 13:04:04 +0900

    Create missing_number.py

---
+++ missing_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:        
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += i - nums[i]
+        
+        return res
