0fad9d2562a00b838f704140ca1e11c57563bbd1
marutaro <niits.git@gmail.com>
2023-03-17 23:45:24 +0900

    Create longest_valid_parentheses.js

---
+++ longest_valid_parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    const stack = [-1];
+    let max_len = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(i);
+        } else {
+            stack.pop();
+            if (stack.length === 0) {
+                stack.push(i);
+            } else {
+                max_len = Math.max(max_len, i - stack[stack.length - 1]);
+            }
+        }
+    }
+
+    return max_len;
+};
