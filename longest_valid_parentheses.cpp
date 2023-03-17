93421bb7f5a6fc5094095aa2e6284787079e5efd
marutaro <niits.git@gmail.com>
2023-03-17 23:51:07 +0900

    Create longest_valid_parentheses.cpp

---
+++ longest_valid_parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        int max_len = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    max_len = max(max_len, i - st.top());
+                }
+            }
+        }
+
+        return max_len;        
+    }
+};
