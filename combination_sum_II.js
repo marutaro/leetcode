18ab43798d758be6da82c01d2b112a0b429fa545
marutaro <niits.git@gmail.com>
2023-03-20 12:02:16 +0900

    Update combination_sum_II.js

--- combination_sum_II.js
+++ combination_sum_II.js
@@ -1,24 +1,36 @@
-  candidates.sort((a, b) => a - b);
-  const res = [];
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    candidates.sort((a, b) => a - b);
+    const res = [];
 
-  function dfs(target, start, comb) {
-    if (target < 0) {
-      return;
-    }
-    if (target === 0) {
-      res.push(comb.slice());
-      return;
-    }
-    for (let i = start; i < candidates.length; i++) {
-      if (i > start && candidates[i] === candidates[i-1]) {
-        continue;
-      }
-      if (candidates[i] > target) {
-        break;
-      }
-      dfs(target - candidates[i], i + 1, comb.concat(candidates[i]));
-    }
-  }
+    function dfs(target, start, comb) {
+        if (target < 0) {
+            return;
+        }
 
-  dfs(target, 0, []);
-  return res;
+        if (target === 0) {
+            res.push(comb.slice());
+            return;
+        }
+        
+        for (let i = start; i < candidates.length; i++) {
+
+            if (i > start && candidates[i] === candidates[i-1]) {
+                continue;
+            }
+
+            if (candidates[i] > target) {
+                break;
+            }
+
+            dfs(target - candidates[i], i + 1, comb.concat(candidates[i]));
+        }
+    }
+
+    dfs(target, 0, []);
+    return res;
+};
