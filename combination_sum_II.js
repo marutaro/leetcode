41b046971b83a16db6db7be1e1cee9d90a24cafb
marutaro <niits.git@gmail.com>
2023-03-20 11:58:41 +0900

    Create combination_sum_II.js

---
+++ combination_sum_II.js
@@ -0,0 +1,24 @@
+  candidates.sort((a, b) => a - b);
+  const res = [];
+
+  function dfs(target, start, comb) {
+    if (target < 0) {
+      return;
+    }
+    if (target === 0) {
+      res.push(comb.slice());
+      return;
+    }
+    for (let i = start; i < candidates.length; i++) {
+      if (i > start && candidates[i] === candidates[i-1]) {
+        continue;
+      }
+      if (candidates[i] > target) {
+        break;
+      }
+      dfs(target - candidates[i], i + 1, comb.concat(candidates[i]));
+    }
+  }
+
+  dfs(target, 0, []);
+  return res;
