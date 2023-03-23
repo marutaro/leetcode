6d7eadddab5c740b4d59d8ea34f5fdd3b6ea2919
marutaro <niits.git@gmail.com>
2023-03-23 18:13:45 +0900

    Create repeated_substring_pattern_1.java

---
+++ repeated_substring_pattern_1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0 && s.substring(0, i).repeat(n / i).equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
