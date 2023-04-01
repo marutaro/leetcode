809199820a4156c01603b231f87244fd389bc4a7
marutaro <niits.git@gmail.com>
2023-04-01 15:48:43 +0900

    Create missing_number.cpp

---
+++ missing_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = nums.size();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            res += i - nums[i];
+        }
+        
+        return res;        
+    }
+};
