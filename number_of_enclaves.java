87fbf1b85f3e898b40c9974ca8443f1a010bcd31
marutaro <niits.git@gmail.com>
2023-04-08 06:24:33 +0900

    Create number_of_enclaves.java

---
+++ number_of_enclaves.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1 && (i == 0 || j == 0 || i == rows - 1 || j == cols - 1)) {
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+
+        return count;        
+    }
+
+    private void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
+            return;
+        }
+
+        grid[i][j] = 2;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+}
