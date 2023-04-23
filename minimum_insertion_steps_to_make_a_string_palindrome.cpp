ba6f4acc4d4a5a243791c144a9e07ade7b07bc32
marutaro <niits.git@gmail.com>
2023-04-23 19:30:31 +0900

    Create minimum_insertion_steps_to_make_a_string_palindrome.cpp

---
+++ minimum_insertion_steps_to_make_a_string_palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.length();
+        vector<int> dp(n, 0);
+
+        for (int i = n - 2; i >= 0; i--) {
+            int prev = 0;
+
+            for (int j = i + 1; j < n; j++) {
+                int temp = dp[j];
+
+                if (s[i] == s[j]) {
+                    dp[j] = prev;
+                } else {
+                    dp[j] = min(dp[j], dp[j-1]) + 1;
+                }
+
+                prev = temp;
+            }
+        }
+
+        return dp[n-1];        
+    }
+};
