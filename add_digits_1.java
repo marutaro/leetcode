ec80ac408aa6aa82ef424790cb4b8bda392af5e6
marutaro <niits.git@gmail.com>
2023-04-26 11:34:27 +0900

    Create add_digits_1.java

---
+++ add_digits_1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        }
+
+        int res = 0;
+        while (num >= 10) {
+            res = 0;
+            while (num != 0) {
+                res += num % 10;
+                num /= 10;
+            }
+            num = res;
+        }
+        
+        return res;        
+    }
+}
