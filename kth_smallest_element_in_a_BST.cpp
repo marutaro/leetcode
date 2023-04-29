f775a27e2d2db26749a233b0aecbb1a517afbcee
marutaro <niits.git@gmail.com>
2023-04-30 00:48:19 +0900

    Create kth_smallest_element_in_a_BST.cpp

---
+++ kth_smallest_element_in_a_BST.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> s;
+
+        while (!s.empty() || root) {
+            while (root) {
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top();
+            s.pop();
+            k--;
+
+            if (k == 0) {
+                return root->val;
+            }
+
+            root = root->right;
+        }
+
+        return -1;        
+    }
+};
