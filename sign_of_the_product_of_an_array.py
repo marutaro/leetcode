3372de9c5f00dc4e8f2b67c41e5fe22c0c686b1b
marutaro <niits.git@gmail.com>
2023-05-03 01:22:50 +0900

    Create sign_of_the_product_of_an_array.py

---
+++ sign_of_the_product_of_an_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        sign = 1
+        for n in nums:
+            if n == 0:
+                return 0
+            if n < 0:
+                sign *= -1
+        
+        return sign
