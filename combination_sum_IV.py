9466cadd18867e21e0191d1ebbb2863e84b774a7
marutaro <niits.git@gmail.com>
2023-05-05 01:27:49 +0900

    Create combination_sum_IV.py

---
+++ combination_sum_IV.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for i in range(1, target + 1):
+            for n in nums:
+                if n <= i:
+                    dp[i] += dp[i-n]
+        
+        return dp[-1]
