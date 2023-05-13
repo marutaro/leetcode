8d10e080d94ea970bd82d2d6d0f2c3fb29448eb0
marutaro <niits.git@gmail.com>
2023-05-14 03:19:18 +0900

    Create count_ways_to_build_good_strings.java

---
+++ count_ways_to_build_good_strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        int[] dp = new int[high+1];
+        dp[0] = 1;
+        int mod = 1000000007;
+
+        for (int i = 1; i <= high; i++) {
+            if (i - zero >= 0) {
+                dp[i] += dp[i-zero];
+            }
+            
+            if (i - one >= 0) {
+                dp[i] += dp[i-one];
+            }
+            
+            dp[i] %= mod;
+        }
+        
+        int res = 0;
+        for (int i = low; i <= high; i++) {
+            res += dp[i];
+            res %= mod;
+        }
+        
+        return res;        
+    }
+}
