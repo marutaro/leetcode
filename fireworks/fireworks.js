6b79b91d69e9b270e6a9c17058f79dd8ca949864
marutaro <niits.git@gmail.com>
2023-05-28 03:29:32 +0900

    Add files via upload

---
+++ fireworks/fireworks.js
@@ -0,0 +1,32 @@
+
+function shower() {
+    // Set the value of Math.Radian
+    
+    // Initialize all variables we use in the code
+
+    // Get the canvas element and its context
+
+    // Set the canvas width and height to match the document dimensions
+
+    // Set the initial positions
+
+    // Determine the maximum length based on the canvas height
+
+    // Select a random color
+
+    // Set up the requestAnimationFrame function
+
+    // Spark object constructor
+
+    // Generate sparks
+    
+    // Set up the canvas styles
+
+    // Animation loop
+
+    // Set interval to change colors every second
+
+    // Set interval to increment step every 300 milliseconds
+
+    // Start the animation loop
+}
