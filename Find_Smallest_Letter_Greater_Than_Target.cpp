70abb57e234a121219719cbe6d76c11ce6806951
marutaro <niits.git@gmail.com>
2023-06-10 04:40:26 +0900

    Create Find_Smallest_Letter_Greater_Than_Target.cpp

---
+++ Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0;
+        int right = letters.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (left == letters.size()) {
+            return letters[0];
+        } else {
+            return letters[left];
+        }        
+    }
+};
