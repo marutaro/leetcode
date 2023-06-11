3d332f6d1bd1436115cbe7b7f4a6a8f3fe59da74
marutaro <niits.git@gmail.com>
2023-06-12 02:44:58 +0900

    Create Snapshot_Array.java

---
+++ Snapshot_Array.java
@@ -0,0 +1,32 @@
+class SnapshotArray {
+    int snapId = 0;
+    TreeMap<Integer, Integer>[] historyRecords;
+    
+    public SnapshotArray(int length) {
+        historyRecords = new TreeMap[length];
+        for (int i = 0; i < length; i++) {
+            historyRecords[i] = new TreeMap<Integer, Integer>();
+            historyRecords[i].put(0, 0);
+        }
+    }
+
+    public void set(int index, int val) {
+        historyRecords[index].put(snapId, val);
+    }
+
+    public int snap() {
+        return snapId++;
+    }
+
+    public int get(int index, int snapId) {
+        return historyRecords[index].floorEntry(snapId).getValue();
+    }
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */
