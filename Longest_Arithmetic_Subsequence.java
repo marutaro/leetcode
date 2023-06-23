979cc981ee111b3f72585ee24481401f63a60844
marutaro <niits.git@gmail.com>
2023-06-24 07:34:58 +0900

    Create Longest_Arithmetic_Subsequence.java

---
+++ Longest_Arithmetic_Subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+
+        Map<Integer, Integer>[] dp = new HashMap[n];
+        int longest = 1;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = new HashMap<>();
+
+            for (int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j];
+                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
+                longest = Math.max(longest, dp[i].get(diff));
+            }
+        }
+
+        return longest;        
+    }
+}
