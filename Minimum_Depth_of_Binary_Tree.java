ca12292e1683e6a98740e61560a68ff6618794c1
marutaro <niits.git@gmail.com>
2023-07-10 12:38:38 +0900

    Create Minimum_Depth_of_Binary_Tree.java

---
+++ Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (left == 0) {
+            return right + 1;
+        } else if (right == 0) {
+            return left + 1;
+        }
+        
+        return Math.min(left, right) + 1;        
+    }
+}
