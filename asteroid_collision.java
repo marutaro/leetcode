b3ee5803b20b5c6832f20ef275e4207eb6ac8475
marutaro <niits.git@gmail.com>
2023-07-20 11:19:23 +0900

    Create asteroid_collision.java

---
+++ asteroid_collision.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int a : asteroids) {
+            if (a > 0) {
+                stack.push(a);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -a) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0)  {
+                    stack.push(a);
+                }
+
+                if (stack.peek() == -a) {
+                    stack.pop();
+                }
+            }
+        }
+
+        int[] res = new int[stack.size()];
+        int i = stack.size() - 1;
+
+        while(!stack.isEmpty()) {
+            res[i--] = stack.pop();
+        }
+
+        return res;   
+    }
+}
