debd7fc7188313656d3e62477a1111224733ae00
marutaro <niits.git@gmail.com>
2023-07-25 02:50:36 +0900

    Create Pow(x, n).java

---
+++ Pow(x, n).java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return binaryExp(x, (long) n);
+    }
+
+    private double binaryExp(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+       
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -n);
+        }
+       
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        } else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+}
