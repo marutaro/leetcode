7729ca5b868707b3d38ea528581527c3aaf94968
marutaro <niits.git@gmail.com>
2023-07-25 12:55:23 +0900

    Create Peak_Index_in_a_Mountain_Array.js

---
+++ Peak_Index_in_a_Mountain_Array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] < arr[mid + 1]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return left;    
+};
