f6bdad39721a11d7c975fc035cd462f5fd3c1016
marutaro <niits.git@gmail.com>
2023-07-25 12:55:54 +0900

    Create Peak_Index_in_a_Mountain_Array.java

---
+++ Peak_Index_in_a_Mountain_Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;        
+    }
+}
