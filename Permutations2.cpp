3cc36e61c6d4f7be864d5e877153596cb1f56ae6
marutaro <niits.git@gmail.com>
2023-08-02 14:03:29 +0900

    Create Permutations2.cpp

---
+++ Permutations2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        backtrack(nums, 0, res);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& res) {
+        if (start == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[start], nums[i]);
+            backtrack(nums, start + 1, res);
+            swap(nums[start], nums[i]);
+        }
+    }
+    
+    void swap(int& a, int& b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }    
+};
