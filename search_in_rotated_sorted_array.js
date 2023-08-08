287106ed535e648f00ef0214ee8206b2e80cc511
marutaro <niits.git@gmail.com>
2023-08-08 11:28:05 +0900

    Update search_in_rotated_sorted_array.js

--- search_in_rotated_sorted_array.js
+++ search_in_rotated_sorted_array.js
@@ -1,23 +1,31 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
 
-        left = 0
-        right = len(nums) - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
 
-        while left <= right:
-            mid = (left + right) // 2
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] >= nums[left]) {
+            if (nums[left] <= target && target <= nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
 
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] >= nums[left]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            else:
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-        
-        return -1
+    return -1;    
+};
