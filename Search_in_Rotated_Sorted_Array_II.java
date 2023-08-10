c18e9d787c0ea17c813c3eb89af5982ecb33d662
marutaro <niits.git@gmail.com>
2023-08-10 13:03:13 +0900

    Create Search_in_Rotated_Sorted_Array_II.java

---
+++ Search_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (nums[mid] == target) {
+                return true;
+            }
+            
+            if (nums[mid] == nums[left]) {
+                left++;
+                continue;
+            }
+            
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return false;        
+    }
+}
