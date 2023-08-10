0d250a0c098c57b8cd1e772df1e3616df90be6bb
marutaro <niits.git@gmail.com>
2023-08-10 13:04:03 +0900

    Create Search_in_Rotated_Sorted_Array_II.cpp

---
+++ Search_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (nums[mid] == target) {
+                return true;
+            }
+            
+            if (nums[mid] == nums[left]) {
+                left++;
+                continue;
+            }
+            
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return false;        
+    }
+};
