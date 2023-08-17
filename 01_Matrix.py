19c7641aef33aa400664d51caee187987020329a
marutaro <niits.git@gmail.com>
2023-08-17 12:16:06 +0900

    Create 01_Matrix.py

---
+++ 01_Matrix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(mat), len(mat[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        queue = deque()
+
+        for r in range(rows):
+            for c in range(cols):
+                if mat[r][c] == 0:
+                    queue.append((r, c))
+                else:
+                    mat[r][c] = float('inf')
+        
+        while queue:
+            row, col = queue.popleft()
+
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+
+                if 0 <= new_row < rows and 0 <= new_col < cols and mat[new_row][new_col] > mat[row][col] + 1:
+                    mat[new_row][new_col] = mat[row][col] + 1
+                    queue.append((new_row, new_col))
+        
+        return mat
