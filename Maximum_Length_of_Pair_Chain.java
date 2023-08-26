359ab51390b89a7bd633ec5a35993748fe9e05dc
marutaro <niits.git@gmail.com>
2023-08-26 10:25:15 +0900

    Create Maximum_Length_of_Pair_Chain.java

---
+++ Maximum_Length_of_Pair_Chain.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[1]));
+
+        int[] prev = pairs[0];
+        int res = 1;
+
+        for (int i = 1; i < pairs.length; i++) {
+            int[] cur = pairs[i];
+            if (cur[0] > prev[1]) {
+                res++;
+                prev = cur;
+            }
+        }
+
+        return res;        
+    }
+}
