dffd37d90c801961439cdae29ec390422c5e74d1
marutaro <niits.git@gmail.com>
2023-08-28 12:27:13 +0900

    Create Implement_Stack_using_Queues_one.cpp

---
+++ Implement_Stack_using_Queues_one.cpp
@@ -0,0 +1,29 @@
+class MyStack {
+private:
+    std::queue<int> q;
+
+public:
+    MyStack() {}
+
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
