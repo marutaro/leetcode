eb593b57aa5b3d4c00a1c520c112c51c06f90bc9
marutaro <niits.git@gmail.com>
2023-09-03 10:42:56 +0900

    Create unique_paths.java

---
+++ unique_paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] aboveRow = new int[n];
+        Arrays.fill(aboveRow, 1);
+
+        for (int row = 1; row < m; row++) {
+            int[] currentRow = new int[n];
+            Arrays.fill(currentRow, 1);
+            for (int col = 1; col < n; col++) {
+                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+            }
+            aboveRow = currentRow;
+        }
+
+        return aboveRow[n - 1];        
+    }
+}
