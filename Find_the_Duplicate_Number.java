f79c619dcbd08adcf355032e0c9baf141250ce11
marutaro <niits.git@gmail.com>
2023-09-19 13:54:00 +0900

    Create Find_the_Duplicate_Number.java

---
+++ Find_the_Duplicate_Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        int slow2 = nums[0];
+
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;        
+    }
+}
