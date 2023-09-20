78f723763a0c99f5ff75163571f3dac91090c86e
marutaro <niits.git@gmail.com>
2023-09-20 15:01:44 +0900

    Create Minimum_Operations_to_Reduce_X_to_Zero.java

---
+++ Minimum_Operations_to_Reduce_X_to_Zero.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int target = 0;
+        for (int num : nums) {
+            target += num;
+        }
+        target -= x;
+
+        if (target < 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int curSum = 0;
+        int maxSubLength = Integer.MIN_VALUE;
+        int n = nums.length;
+
+        for (int right = 0; right < n; right++) {
+            curSum += nums[right];
+
+            while (curSum > target) {
+                curSum -= nums[left];
+                left++;
+            }
+
+            if (curSum == target) {
+                maxSubLength = Math.max(maxSubLength, right - left + 1);
+            }
+        }
+
+        return maxSubLength == Integer.MIN_VALUE ? -1 : n - maxSubLength;        
+    }
+}
