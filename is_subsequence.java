e200ffa1f1d67a21036a76a10a8e14b2a3576e0b
marutaro <niits.git@gmail.com>
2023-09-22 13:03:11 +0900

    Create is_subsequence.java

---
+++ is_subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sIdx = 0;
+        int tIdx = 0;
+
+        while (sIdx < s.length() && tIdx < t.length()) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+            }
+            tIdx++;
+        }
+
+        return sIdx == s.length();        
+    }
+}
