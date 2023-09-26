a047893721470665135512e79f27d9a6aa87c87a
marutaro <niits.git@gmail.com>
2023-09-26 15:12:41 +0900

    Create Remove_Duplicate_Letters.js

---
+++ Remove_Duplicate_Letters.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters = function(s) {
+    const lastOccurrence = {};
+    for (let i = 0; i < s.length; i++) {
+        lastOccurrence[s[i]] = i;
+    }
+
+    const stack = [];
+    const visited = new Set();
+
+    for (let i = 0; i < s.length; i++) {
+        if (visited.has(s[i])) {
+            continue;
+        }
+
+        while (
+            stack.length > 0 &&
+            s[i] < stack[stack.length - 1] &&
+            i < lastOccurrence[stack[stack.length - 1]]
+        ) {
+            visited.delete(stack.pop());
+        }
+
+        visited.add(s[i]);
+        stack.push(s[i]);
+    }
+
+    return stack.join('');    
+};
