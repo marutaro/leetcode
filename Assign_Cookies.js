7dfab8bdbcf585cef74ab3c690031f20166a920b
marutaro <niits.git@gmail.com>
2024-01-01 14:39:06 +0900

    Create Assign_Cookies.js

---
+++ Assign_Cookies.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+
+    let i = 0;
+    let j = 0;
+
+    while (i < g.length && j < s.length) {
+        if (g[i] <= s[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return i;    
+};
