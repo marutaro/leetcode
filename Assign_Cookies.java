1ddada67fb437af3371f092799c034d7054c520f
marutaro <niits.git@gmail.com>
2024-01-01 14:39:37 +0900

    Create Assign_Cookies.java

---
+++ Assign_Cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i;        
+    }
+}
