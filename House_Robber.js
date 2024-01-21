30e8cc01dca101f1e61e800ff38c082112cbd58e
marutaro <niits.git@gmail.com>
2024-01-22 00:11:33 +0900

    Update House_Robber.js

--- House_Robber.js
+++ House_Robber.js
@@ -1,7 +1,24 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+// O(n) time, O(n) space
+var rob = function(nums) {
+    const n = nums.length;
+
+    if (n === 1) {
+        return nums[0];
+    }
+
+    const dp = Array(n).fill(0);
+
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (let i = 2; i < n; i++) {
+        dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+    }
+
+    return dp[n - 1];    
+};
+
+// O(n) time, O(1) space
 var rob = function(nums) {
     let prevRob = 0;
     let maxRob = 0;
