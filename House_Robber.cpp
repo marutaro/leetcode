bf49fe3a608fc35cf1bd22fc8ff8e95e61f2f0d5
marutaro <niits.git@gmail.com>
2024-01-22 00:16:19 +0900

    Update House_Robber.cpp

--- House_Robber.cpp
+++ House_Robber.cpp
@@ -1,3 +1,27 @@
+// O(n) time, O(n) space 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        vector<int> dp(n, 0);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];        
+    }
+};
+
+// O(n) time, O(1) space 
 class Solution {
 public:
     int rob(vector<int>& nums) {
