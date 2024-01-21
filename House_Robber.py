cf51658c8f8474d9ccedea58c2e841fefaee5c78
marutaro <niits.git@gmail.com>
2024-01-22 00:09:39 +0900

    Update House_Robber.py

--- House_Robber.py
+++ House_Robber.py
@@ -1,3 +1,22 @@
+# O(n) time, O(n) space
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * n
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        
+        return dp[-1] 
+
+# O(n) time, O(1) space
 class Solution:
     def rob(self, nums: List[int]) -> int:
         prev_rob = max_rob = 0
