a4f0c113a7ffcac80ae28faf35e3fcfc040b2ca9
marutaro <niits.git@gmail.com>
2024-01-22 00:14:05 +0900

    Update House_Robber.java

--- House_Robber.java
+++ House_Robber.java
@@ -1,3 +1,26 @@
+// O(n) time, O(n) space
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];        
+    }
+}
+
+// O(n) time, O(1) space
 class Solution {
     public int rob(int[] nums) {
         int prevRob = 0;
