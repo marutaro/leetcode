e62779ec6bb258236976675a17b3b96158c889b3
marutaro <niits.git@gmail.com>
2024-01-31 15:20:19 +0900

    Create Daily_Temperatures.java

---
+++ Daily_Temperatures.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        return result;        
+    }
+}
