5813c208060176327a1c840e86cab2676b1c3a2b
marutaro <niits.git@gmail.com>
2024-02-17 15:45:39 +0900

    Create Furthest Building You Can Reach.cpp

---
+++ Furthest Building You Can Reach.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for (int i = 1; i < heights.size(); ++i) {
+            int diff = heights[i] - heights[i - 1];
+
+            if (diff <= 0)
+                continue;
+
+            minHeap.push(diff);
+
+            if (minHeap.size() > ladders)
+                bricks -= minHeap.top(), minHeap.pop();
+            if (bricks < 0)
+                return i - 1;
+        }
+
+        return heights.size() - 1;        
+    }
+};
