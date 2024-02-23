121210a11d959c9fc4eb6d9d4279b9b80f20b4fa
marutaro <niits.git@gmail.com>
2024-02-24 03:06:07 +0900

    Create Valid_Anagram.java

---
+++ Valid_Anagram.java
@@ -0,0 +1,68 @@
+// Solution 1
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> counter = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
+                return false;
+            }
+            counter.put(ch, counter.get(ch) - 1);
+        }
+
+        return true;        
+    }
+}
+
+// Solution 2
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a'] += 1;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (count[t.charAt(i) - 'a'] == 0) {
+                return false;
+            }
+            count[t.charAt(i) - 'a'] -= 1;
+        }
+
+        return true;        
+    }
+}
+
+// Solution 3
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        HashMap<Character, Integer> tCount = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sCount.put(s.charAt(i), 1 + sCount.getOrDefault(s.charAt(i), 0));
+            tCount.put(t.charAt(i), 1 + tCount.getOrDefault(t.charAt(i), 0));
+        }
+
+        return sCount.equals(tCount);        
+    }
+}
