3b81e39cb7f199a9dcb03b5357244204020128ff
marutaro <niits.git@gmail.com>
2024-02-26 11:35:15 +0900

    Create Same_Tree.js

---
+++ Same_Tree.js
@@ -0,0 +1,21 @@
+// Solution 1
+var isSameTree = function(p, q) {
+    if (!p && !q) {
+        return true;
+    }
+    
+    if (p && q && p.val === q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    
+    return false;    
+};
+
+// Solution 2
+var isSameTree = function(p, q) {
+    if (!p || !q) {
+        return p === q;
+    }
+    
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);    
+};
