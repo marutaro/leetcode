b782f6c4cb4fb28f0d8605f1972631e7decd3b7a
marutaro <niits.git@gmail.com>
2024-02-26 11:36:16 +0900

    Create Same_Tree.java

---
+++ Same_Tree.java
@@ -0,0 +1,26 @@
+// Solution 1
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) {
+            return true;
+        }
+        
+        if (p && q && p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        
+        return false;        
+    }
+};
+
+// Solution 2
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);        
+    }
+}
