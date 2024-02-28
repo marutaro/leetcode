e5ce69c07df14a858beb9de084f3c10f8c2f156a
marutaro <niits.git@gmail.com>
2024-02-29 03:41:44 +0900

    Create Kth_Largest_Element_in_a_Stream.java

---
+++ Kth_Largest_Element_in_a_Stream.java
@@ -0,0 +1,20 @@
+class KthLargest {
+    private int k;
+    private PriorityQueue<Integer> minHeap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            add(num);
+        }        
+    }
+    
+    public int add(int val) {
+        minHeap.offer(val);
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
